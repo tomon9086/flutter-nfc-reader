d49fef8bcfedec3c7d0f5c578b0906ccddf119f5
Takeshi Tanno <t-tanno@silvereye.jp>
2019-11-12 15:01:34 +0900

    androidでビルドが通るようにした

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tomo/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tomo/Programing/Private/flutter-nfc-reader/example"
-export "FLUTTER_TARGET=/Users/tomo/Programing/Private/flutter-nfc-reader/example/lib/main.dart"
+export "FLUTTER_ROOT=/Users/silvereye_mac01/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/silvereye_mac01/Documents/tomo_repositories/flutter-nfc-reader/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/tomo/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/silvereye_mac01/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
